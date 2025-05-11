151bafca3399fbd9bcdae4de5e3dc5c83fbef7e5
Pedro <pcapolozano@cifpfbmoll.eu>
2025-05-12 00:12:46 +0200

    Cosas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { SeparatorComponent } from './components/separator/separator.component';
+import { SpecialButtonsComponent } from './components/special-buttons/special-buttons.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HomeHeaderComponent } from './pages/home-header/home-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, FooterComponent, SeparatorComponent, SpecialButtonsComponent, HomeComponent, HomeHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'BattleFantasy';
 }
---
+++ src/app/components/special-buttons/special-buttons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-special-buttons',
+  imports: [],
+  templateUrl: './special-buttons.component.html',
+  styleUrl: './special-buttons.component.scss'
+})
+export class SpecialButtonsComponent {
+
+}
